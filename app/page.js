e02416dbd67e0aa5edf4be8a623b04c609c5933a
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-07-09 23:03:17 +0600

    🛠️📕header footer and sections added

--- app/page.js
+++ app/page.js
@@ -1,9 +1,25 @@
+import Footer from "@/components/layout/Footer";
+import Header from "@/components/layout/Header";
+import Problems from "@/views/Problems";
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main>
-      <div className=""></div>
-    </main>
+    <>
+      <Header />
+
+      <main>
+        <section id="hero">
+          <div className="timeline"></div>
+        </section>
+        <section id="scrolling"></section>
+        <Problems />
+        <section id="layout"></section>
+        <section id="transition"></section>
+        <section id="caveats"></section>
+      </main>
+
+      <Footer />
+    </>
   );
 }
