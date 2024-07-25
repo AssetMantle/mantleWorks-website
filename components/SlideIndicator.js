4e845a4b071a3ca51091c10df4b5a15dcec2acbb
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-07-25 15:30:34 +0600

    🛠️🦄💅📕🏷️Slide indicator component created & completed

---
+++ components/SlideIndicator.js
@@ -0,0 +1,44 @@
+import React from "react";
+
+export default function SlideIndicator({ indicator }) {
+  const sectionMap = [
+    {
+      style: { "--indicator-bg": "var(--orange)" },
+    },
+    {
+      style: { "--indicator-bg": "var(--red)" },
+    },
+    {
+      style: { "--indicator-bg": "var(--pink)" },
+    },
+    {
+      style: { "--indicator-bg": "var(--purple)" },
+    },
+    {
+      style: { "--indicator-bg": "var(--blue)" },
+    },
+    {
+      style: { "--indicator-bg": "var(--green)" },
+    },
+  ];
+
+  return (
+    <div className="am-mw-slideIndicator">
+      {React.Children.toArray(
+        sectionMap.map((el, ind) => (
+          <button
+            className={`indicator mw-${indicator}${
+              indicator === ind + 1 ? " active" : ""
+            }`}
+            style={el?.style}
+            onClick={() =>
+              window &&
+              window.innerHeight &&
+              window.scrollTo(0, window.innerHeight * (ind + 1))
+            }
+          ></button>
+        ))
+      )}
+    </div>
+  );
+}
