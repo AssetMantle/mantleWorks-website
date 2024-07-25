d30af244156908634ef3590043febf5e6538ae45
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-07-25 15:30:08 +0600

    🛠️Slider indicator added

--- app/page.js
+++ app/page.js
@@ -1,11 +1,28 @@
+"use client";
 import Footer from "@/components/layout/Footer";
 import Header from "@/components/layout/Header";
+import SlideIndicator from "@/components/SlideIndicator";
 import Assetization from "@/views/Assetization";
 import Hero from "@/views/Hero";
 import Problems from "@/views/Problems";
 import WhatWeDo from "@/views/WhatWeDo";
+import { useEffect, useState } from "react";
 
 export default function Home() {
+  const [Position, setPosition] = useState(0);
+
+  useEffect(() => {
+    if (window) {
+      window.addEventListener("scroll", () => {
+        window.innerHeight &&
+          window.scrollY &&
+          setPosition(Math.round(window.scrollY / window.innerHeight));
+      });
+    }
+  }, []);
+
+  console.log(Position);
+
   return (
     <>
       <Header />
@@ -18,6 +35,7 @@ export default function Home() {
         <Assetization />
       </main>
 
+      <SlideIndicator indicator={Position} />
       <Footer />
     </>
   );
