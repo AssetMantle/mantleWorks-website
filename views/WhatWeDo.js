b9f1eaf8105a2fbaa601b09f1ad37d557cdc50d4
AssetMantle DevOps <devops@assetmantle.one>
2024-09-04 17:44:52 +0530

    fixed video full-screen bug

--- views/WhatWeDo.js
+++ views/WhatWeDo.js
@@ -26,32 +26,13 @@ export default function WhatWeDo() {
       <div className="am-mw-container am-mw-work d-flex justify-content-center align-items-center flex-column h-100">
         <h2 className={`${montserrat.className}`}>What We Do</h2>
         <div className="video">
-          {/* <video
-            ref={videoRef}
-            src="https://videos.pexels.com/video-files/6583334/6583334-uhd_2732_1318_30fps.mp4"
-            poster="/media/bgs/work.png"
-            onEnded={handleOnEnd}
-            controlsList="nodownload nofullscreen noremoteplayback"
-          ></video>
-          <button
-            ref={buttonRef}
-            className="am-mw-bg-gradient-red-blue d-flex align-items-center justify-content-center"
-            onClick={handlePlay}
-          >
-            <Image
-              src="/media/icons/play-fill.svg"
-              alt="play icon"
-              width={50}
-              height={50}
-            />
-          </button> */}
           <iframe
             src="https://www.youtube-nocookie.com/embed/RJ1vxXeKgZ0?si=jsifRxs4iQJhDN35"
             title="YouTube video player"
-            frameborder="0"
+            frameBorder="0"
             allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-            referrerpolicy="strict-origin-when-cross-origin"
-            allowfullscreen
+            referrerPolicy="strict-origin-when-cross-origin"
+            allowFullScreen
           ></iframe>
         </div>
       </div>
