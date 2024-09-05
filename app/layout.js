9f1a2d92094f6ef8ce4ec4b2c658b988823e4925
Akhlak Hossain Jim <73884856+Akhlak-Hossain-Jim@users.noreply.github.com>
2024-09-05 13:14:11 +0600

    🛠️📕og content added

--- app/layout.js
+++ app/layout.js
@@ -10,6 +10,19 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="theme-color" content="#ffffff" />
+        {/* open graphs start */}
+        <meta property="og:site_name" content="MantleWorks" />
+        <meta property="og:url" content="https://mantleworks.one" />
+        <meta property="al:web:url" content="https://mantleworks.one" />
+        <meta property="og:image" content="/og.png" />
+        <meta property="og:title" content="MantleWorks" />
+        <meta
+          property="og:description"
+          content="The institutional assetization arm of AssetMantle"
+        />
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
